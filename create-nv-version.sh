491ad1c6f45ac19a0210186f6ac82a58adbb1a6d
mariojmdavid <mariojmdavid@gmail.com>
2016-11-04 17:29:33 +0000

    add comments on change files

--- create-nv-version.sh
+++ create-nv-version.sh
@@ -18,4 +18,13 @@ BRANCHES="master devel"
 
 git pull
 
+# Change files dependent on nvidia version
+# defaults/main.yml:# 352.99 352.93 352.79 352.68 352.63 352.55
+# docker-nvidia/ubuntu16.04/Dockerfile:RUN ansible-galaxy install LIP-Computing.ansible-role-nvidia,nvdrv_352.93 && \
+# docker-nvidia/ubuntu16.04/README.md:* TAG = nvdrv_352.93 : nvidia driver version 352.93
+# docker-nvidia/centos7/Dockerfile:RUN ansible-galaxy install LIP-Computing.ansible-role-nvidia,nvdrv_352.93 && \
+# docker-nvidia/centos7/README.md:* TAG = nvdrv_352.93 : nvidia driver version 352.93
+# README.md:Playbook example: deployment of driver version 352.93 on a VM
+# README.md:        - { role: LIP-Computing.ansible-role-nvidia, nvidia_driver_version: 352.93, kmod_install: true }
+# tests/install-docker.yml:    - { role: LIP-Computing.ansible-role-nvidia, nvidia_driver_version: 352.93, kmod_install: false }
 
